84f4c82a57241d5cc10521161196d6e518b73317
airamdl <airamdl2001@gmail.com>
2025-04-09 10:33:08 +0100

    kmp basico

---
+++ ComposeDemo/composeApp/src/desktopMain/kotlin/compose/project/demo/Platform.jvm.kt
@@ -0,0 +1,8 @@
+package compose.project.demo
+
+class JVMPlatform: Platform {
+    override val name: String = "Java ${System.getProperty("java.version")}"
+
+}
+
+actual fun getPlatform(): Platform = JVMPlatform()
---
+++ ComposeDemo/composeApp/src/desktopMain/kotlin/compose/project/demo/main.kt
@@ -0,0 +1,14 @@
+package compose.project.demo
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "ComposeDemo",
+    ) {
+        App()
+
+    }
+}
